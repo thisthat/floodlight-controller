14b193bfc795857d359c7f9df2c996446f71e82f
Giovanni Liva <this_that@hotmail.it>
2015-06-02 09:11:34 +1200

    Create the POST route handler

--- src/main/java/net/floodlightcontroller/prediction/TopologyGraphResource.java
+++ src/main/java/net/floodlightcontroller/prediction/TopologyGraphResource.java
@@ -1,11 +1,12 @@
 package net.floodlightcontroller.prediction;
 
-import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
-
+import org.json.JSONObject;
 import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 
 public class TopologyGraphResource extends ServerResource {
 	@Get("json")
@@ -16,8 +17,16 @@ public class TopologyGraphResource extends ServerResource {
 	
 	@Post
 	public String store(String in) {
-		INetTopologyService pihr = (INetTopologyService)getContext().getAttributes().get(INetTopologyService.class.getCanonicalName());
-        return pihr.getTopologyGraph(in);
+		INetTopologyService service = (INetTopologyService)getContext().getAttributes().get(INetTopologyService.class.getCanonicalName());
+		try {
+			JSONObject jp = new JSONObject(in);
+			String type = jp.getString("type");
+			return service.getTopologyGraph(type);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+			return "{ \"status\" : \"err\" }\n";
+		}
 	}
 	
 }
